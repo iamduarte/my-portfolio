988ef8b5495d6f1e2f5eba3d2f361123103a1798
Duarte M <64815777+ozzypt@users.noreply.github.com>
2023-10-27 18:30:59 +0100

    added cv link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -42,39 +42,52 @@ export default function Work() {
     <>
       <div className="hero min-h-screen">
         <div className="hero-overlay bg-orange-950 bg-opacity-40"></div>
-        <div className="mask2 bg-customBgColor w-full h-full flex items-center lg:justify-evenly flex-col lg:flex-row justify-center">
-          <h1 className="text-gray-100 text-6xl  text-center px-5">
-            Some of my work
-          </h1>
-          <p
-            className="text-gray-100 text-2xl mt-20 lg:mt-8 pb-6 cursor-pointer"
-            onClick={() =>
-              openModal(
-                works[0].title,
-                works[0].description,
-                works[0].altImage,
-                works[0].image,
-                works[0].url
-              )
-            }
+        <div className="mask2 bg-customBgColor w-full h-full items-center flex flex-col justify-center">
+          <div className="flex flex-col lg:flex-row w-screen justify-evenly">
+            <h1 className="text-gray-100 text-6xl text-center px-5 self-center">
+              Some of my work
+            </h1>
+            <p
+              className="text-gray-100 text-2xl mt-20 lg:mt-8 pb-6 cursor-pointer self-center"
+              onClick={() =>
+                openModal(
+                  works[0].title,
+                  works[0].description,
+                  works[0].altImage,
+                  works[0].image,
+                  works[0].url
+                )
+              }
+            >
+              PR Manager Portfolio Website
+            </p>
+            <p
+              className="text-gray-100 text-2xl lg:mt-8 pb-6 cursor-pointer self-center"
+              onClick={() =>
+                openModal(
+                  works[1].title,
+                  works[1].description,
+                  works[1].altImage,
+                  works[1].image,
+                  works[1].url
+                )
+              }
+            >
+              HelpUkraineHub
+            </p>
+          </div>
+          <a
+            href="https://drive.google.com/file/d/1h0rNErAuWuMcLIUMiauRiPiWj1YK1bMx/view?usp=drive_link"
+            aria-label="donwload cv"
+            target="_blank"
+            rel="noreferrer"
           >
-            PR Manager Portfolio Website
-          </p>
-          <p
-            className="text-gray-100 text-2xl lg:mt-8 pb-6 cursor-pointer"
-            onClick={() =>
-              openModal(
-                works[1].title,
-                works[1].description,
-                works[1].altImage,
-                works[1].image,
-                works[1].url
-              )
-            }
-          >
-            HelpUkraineHub
-          </p>
+            <p className="text-gray-100 text-2xl cursor-pointer lg:mt-24 mt-10">
+              Check out my CV
+            </p>
+          </a>
         </div>
+
         <div className="mask3 bg-customBgColor w-full h-full"></div>
         {isModalOpen && (
           <Modal
